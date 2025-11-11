baf060c1b36013ff23280e026c296c21e7423b5f
ShumikDV <dimitr227@gmail.com>
2025-11-11 00:51:54 +0000

    test(e2e): drop duplicate viewport; add build step; universal UI check; Telegram Mini App tests

---
+++ tests/e2e/telegram-types.d.ts
@@ -0,0 +1,25 @@
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        ready: () => void;
+        expand: () => void;
+        close: () => void;
+        BackButton: {
+          isVisible: boolean;
+          show: () => void;
+          hide: () => void;
+        };
+        MainButton: {
+          text: string;
+          show: () => void;
+          hide: () => void;
+          onClick: (cb: () => void) => void;
+          offClick: (cb: () => void) => void;
+        };
+      };
+    };
+  }
+}
+
+export {};
